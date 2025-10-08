f69383479dc65cdc89a9f2137c370c55e43ca4a3
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-10-08 18:35:11 +0530

    feat : animated the navbar with motion/react

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Container } from "../container";
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "motion/react";
 
 export const Navbar = () => {
+  const [hoveredItem, setHoveredItem] = useState<number | null>(null);
   const navItems = [
     {
       title: "About",
@@ -22,6 +26,7 @@ export const Navbar = () => {
       href: "/blog",
     },
   ];
+
   return (
     <Container>
       <nav className="flex items-center justify-between p-2">
@@ -34,9 +39,21 @@ export const Navbar = () => {
         />
 
         <div className="flex items-center gap-4">
-          {navItems.map((item) => (
-            <Link className="text-sm" href={item.href} key={item.title}>
-              {item.title}
+          {navItems.map((item, idx) => (
+            <Link
+              className="relative px-2 py-1 text-sm"
+              href={item.href}
+              key={idx}
+              onMouseEnter={() => setHoveredItem(idx)}
+              onMouseLeave={() => setHoveredItem(null)}
+            >
+              {hoveredItem === idx && (
+                <motion.span
+                  layoutId="hovered-span"
+                  className="absolute inset-0 h-full w-full rounded-md bg-neutral-100 dark:bg-neutral-700"
+                />
+              )}
+              <span className="relative z-10">{item.title}</span>
             </Link>
           ))}
         </div>
