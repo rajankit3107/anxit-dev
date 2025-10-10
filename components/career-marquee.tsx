5505ade0e6cfa3f4dba60a538ccfe626d2b73e9c
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-10-10 18:23:58 +0530

    added images to the collage

--- components/career-marquee.tsx
+++ components/career-marquee.tsx
@@ -4,19 +4,19 @@ import Marquee from "react-fast-marquee";
 
 export default function Career() {
   const data = [
-    { src: "/projects/shopify.png" },
-    { src: "/projects/shopify.png" },
-    { src: "/projects/shopify.png" },
-    { src: "/projects/shopify.png" },
-    { src: "/projects/shopify.png" },
-    { src: "/projects/shopify.png" },
-    { src: "/projects/shopify.png" },
+    { src: "/travel/project_showcase.jpeg" },
+    { src: "/travel/table.jpeg" },
+    { src: "/travel/banner.jpeg" },
+    { src: "/travel/friends.jpeg" },
+    { src: "/travel/friendsonbike.jpeg" },
+    { src: "/travel/group.jpeg" },
+    { src: "/travel/park.jpeg" },
   ];
 
   return (
     <div className="py-10">
-      <div className="flex [mask-image:linear-gradient(to_right,rgba(0,0,0,0.1),rgba(0,0,0,1)_20%,rgba(0,0,0,1)_80%,rgba(0,0,0,0.1))]">
-        <Marquee speed={50} pauseOnHover className="py-4">
+      <div className="flex">
+        <Marquee speed={50} pauseOnHover className="py-2">
           {data.map((item, idx) => (
             <CareerCard index={idx} key={idx} {...item} />
           ))}
@@ -28,10 +28,14 @@ export default function Career() {
 
 const CareerCard = ({ src, index }: { src: string; index: number }) => {
   return (
-    <div className="shadow-custom mx-4 flex h-50 w-full max-w-60 flex-col justify-between gap-4 rounded-xl p-4 transition duration-300 hover:shadow-xl">
-      <div className="flex items-center gap-4">
-        <Image src={src} alt="No Image Found" width={600} height={600} />
-      </div>
+    <div className="shadow-custom mx-2 flex h-40 w-auto max-w-[250px] flex-col justify-center overflow-hidden rounded-md transition duration-300 hover:shadow-xl">
+      <Image
+        src={src}
+        alt="No Image Found"
+        width={600}
+        height={600}
+        className="h-full w-full object-cover"
+      />
     </div>
   );
 };
